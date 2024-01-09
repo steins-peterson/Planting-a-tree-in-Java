9fa0a0e13dfa76ad6d4d2923defdce139e049fd6
Steins Peterson <petersonsteins@gmail.com>
2024-01-09 17:42:37 +0530

    Initial commit

---
+++ src/Tree.java
@@ -0,0 +1,12 @@
+public class Tree{
+    public static void main(String[] args){
+        // introduce ourselves and print a tree
+        System.out.println("Hey there, I'm Steins and I'm learning to code in Java!");
+        System.out.println("I'm ready to get my hands dirty");
+        System.out.println("  *   ");
+        System.out.println(" *** ");
+        System.out.println("*****");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
